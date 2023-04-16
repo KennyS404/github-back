0fc2ffca5c3f7b37425975da87fc4c65b458162b
KennyS404 <kennyldo48@gmail.com>
2023-04-16 02:09:25 -0300

    url changed to the render url

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ app.get('/api/user/repos', async (req, res) => {
 })
 
 
-const port = 9000
+const port = process.env.PORT || 9000
 app.listen(port, () => console.log(`Server running at port ${port}`))
